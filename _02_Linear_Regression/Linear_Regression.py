6ec3580c959e84f91b31c46d3f7a8191f80a7239
954310027 <954310027@qq.com>
2023-05-05 00:11:34 +0800

    test3

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -38,7 +38,59 @@ def ridge(xArr, yArr):
 
 
 def lasso(X, y, lambdas=0.1, max_iter=1000, tol=1e-4):
-    pass
+    def down(X, y, w, index, lambdas=0.1):
+        # 展开后的二次项的系数之和
+        aa = 0
+        # 展开后的一次项的系数之和
+        ab = 0
+        for i in range(X.shape[0]):
+            # 括号内一次项的系数
+            a = X[i][index]
+            # 括号内常数项的系数
+            b = X[i][:].dot(w) - a * w[index] - y[i]
+            # 可以很容易的得到展开后的二次项的系数为括号内一次项的系数平方的和
+            aa = aa + a * a
+            # 可以很容易的得到展开后的一次项的系数为括号内一次项的系数乘以括号内常数项的和
+            ab = ab + a * b
+        # 由于是一元二次函数，当导数为零时，函数值最小值，只需要关注二次项系数、一次项系数和 λ
+        return det(aa, ab, lambdas)
+
+    def det(aa, ab, lambdas=0.1):
+        w = - (2 * ab + lambdas) / (2 * aa)
+        if w < 0:
+            w = - (2 * ab - lambdas) / (2 * aa)
+            if w > 0:
+                w = 0
+        return w
+
+    """
+            Lasso回归，使用坐标下降法（coordinate descent）
+            args:
+                X - 训练数据集
+                y - 目标标签值
+                lambdas - 惩罚项系数
+                max_iter - 最大迭代次数
+                tol - 变化量容忍值
+            return:
+                w - 权重系数
+            """
+    # 初始化 w 为零向量
+    w = np.zeros(X.shape[1])
+    for it in range(max_iter):
+        done = True
+        # 遍历所有自变量
+        for i in range(0, len(w)):
+            # 记录上一轮系数
+            weight = w[i]
+            # 求出当前条件下的最佳系数
+            w[i] = down(X, y, w, i, lambdas)
+            # 当其中一个系数变化量未到达其容忍值，继续循环
+            if (np.abs(weight - w[i]) > tol):
+                done = False
+        # 所有系数都变化不大时，结束循环
+        if (done):
+            break
+    return w
 
 
 def read_data(path='./data/exp02/'):
@@ -47,9 +99,8 @@ def read_data(path='./data/exp02/'):
     return x, y
 
 
-def main(data):
-    x = read_data()[0]
-    y = read_data()[1]
+def main():
+    x, y = read_data()
     weight1 = ridge(x, y)
     #weight2 = lasso(x, y)
     return weight1
